2530ebbc54382b7c5fb8aeacdb3c648c63d30e5f
Maha070411 <srmaha074@gmail.com>
2026-02-26 15:20:04 +0530

    initial commit

---
+++ maha/src/test/java/com/sample/maha/MahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.maha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
